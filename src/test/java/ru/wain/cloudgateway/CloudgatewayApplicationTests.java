0bc0438b4413d971c768f7eafe0ca43ea9e56f6e
wain_m1 <vokash3@yandex.ru>
2022-07-23 15:14:22 +0300

    Initial commit

---
+++ src/test/java/ru/wain/cloudgateway/CloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wain.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
